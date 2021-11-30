a41a8479a30d1cc3ba81b80d36b6b5e6c75509a4
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-30 14:24:07 -0600

    Add project files.

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kanyeUrl = "https://api.kanye.rest";
+
+            var client = new HttpClient();
+
+            var kanyeResponse = client.GetStringAsync(kanyeUrl).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine(kanyeQuote);
+            Console.ReadLine();
+            var swansonUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var clientTwo = new HttpClient();
+
+            var ronResponse = clientTwo.GetStringAsync(swansonUrl).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine(ronQuote);
+            Console.ReadLine();
+        }
+    }
+}
